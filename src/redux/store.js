b71549a334ebd56ad986749ffb4bac827b7b43c0
SyvonenkoOleh <115041334+SyvonenkoOleh@users.noreply.github.com>
2023-06-20 18:43:16 +0300

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import { contactsSlice } from './contactsSlice';
 import { filterSlice } from './filterSlice';
 
 export const store = configureStore({
+  
   reducer: {
     contacts: contactsSlice.reducer,
     filter: filterSlice.reducer,
